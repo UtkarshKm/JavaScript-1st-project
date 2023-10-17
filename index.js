b5a3fb19cfae11c1ced1b2b268edddf9e6601579
Utkarsh Kumawat <135964893+UtkarshKm@users.noreply.github.com>
2023-10-17 16:39:20 +0530

    added comments

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
+// Initializing the player's details
 let player = {
     name: "Utkarsh",
     chips: 200
 }
 
+// Initializing necessary variables for the game
 let cards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
+
+// Retrieving necessary HTML elements
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 
+// Displaying the player's name and chips
 playerEl.textContent = player.name + ": $" + player.chips
 
+// Function to get a random card value
 function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    let randomNumber = Math.floor(Math.random() * 13) + 1
     if (randomNumber > 10) {
-        return 10
+        return 10 // Assign 10 for Jack, Queen, and King
     } else if (randomNumber === 1) {
-        return 11
+        return 11 // Ace is initially assigned 11 points
     } else {
         return randomNumber
     }
 }
 
+// Function to start the game
 function startGame() {
     isAlive = true
     let firstCard = getRandomCard()
@@ -35,6 +42,7 @@ function startGame() {
     renderGame()
 }
 
+// Function to render the game state
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
@@ -54,7 +62,7 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-
+// Function to draw a new card
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
